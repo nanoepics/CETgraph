900f76ae5914919fb2791e592a4f024f7495cd4e
peterspeets <36505001+peterspeets@users.noreply.github.com>
2018-04-04 16:04:56 +0200

    Added comments
    Added comments

--- tracking/trackParticles.py
+++ tracking/trackParticles.py
@@ -13,6 +13,7 @@ This script uses the tracking class to obtain particle data from imaged frames
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas
 import trackpy as tp
 import pims
 import math
@@ -21,13 +22,21 @@ import os
 import datetime
 import h5py
 import cv2
+import pickle
 import sys #gives sys.exit() for debugging
 from tracking import Tracking
 
 
 
-folder  = "D:\\Onderzoek\\data\\19-03-2018\\run1" 
-trackingObject = Tracking(folder, 21, 10000, 40000, 5, 0.225664)
+folder  = "D:\\Onderzoek\\data\\19-03-2018\\run4" 
+
+"""
+create a tracking object.
+tracking(path to data, particle diameter (px), minmass, maxmass, 
+min number of frames for detected particle,micron per pixel)
+"""
+
+trackingObject = Tracking(folder, 31, 1200, 4000, 5, 0.225664)
 folder = trackingObject.currentPath
 trackingObject.saveImage(trackingObject.frames[0], folder + "/firstFrame.png")
 
@@ -36,13 +45,14 @@ trackingObject.subtractBackground()
 trackingObject.saveAVIData(trackingObject.frames, folder + "/subtractedBackground")
 trackingObject.showDetectedParticles()
 
-trackingObject.maxFrames = 10
-trackingObject.detectParticles()
-trackingObject.linkParticles()
-trackingObject.calculateDiffusion()
-trackingObject.generateMiscPlots()
+trackingObject.maxFrames = -1 #max frames used. if -1 all frames will be used.
+trackingObject.minimumMSD = 0.2 #minimum mean square displacement. Use to prevent stuck particles.
+trackingObject.detectParticles() #diagnostic function. plot detected particles of first frame
+trackingObject.linkParticles() #link different frames
 
 
-
+trackingObject.calculateDiffusion(maxLagTime = 5) #maxLagTime is how many frames will be used per fit
+trackingObject.generateMiscPlots(binsize = 25) #generate histograms of data
+trackingObject.getSettings()   #write to metadata
 
 
